e3cda92b72c110f2e2ff6ad9ba064e6567f5fe2c
rivero04 <7710@holbertonstudents.com>
2024-04-17 07:04:35 -0700

    Init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Initialises views package"""
 
-from api.v1.views.index import *
-from .states import *
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
